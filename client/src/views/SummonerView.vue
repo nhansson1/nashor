ebe12474840b1c0250fc88b7198b43010cece30e
nhansson1 <niclaskalleby@gmail.com>
2025-05-15 22:15:27 +0200

    replaced localhost with env variable

--- client/src/views/SummonerView.vue
+++ client/src/views/SummonerView.vue
@@ -6,9 +6,9 @@ import type { Account, Summoner, RankEntry } from "@/types/league";
 import MatchHistory from "@/components/MatchHistory.vue";
 
 interface ProfileResponse {
-  account: Account;
-  summoner: Summoner;
-  ranks: RankEntry[];
+    account: Account;
+    summoner: Summoner;
+    ranks: RankEntry[];
 }
 
 const router = useRouter();
@@ -16,66 +16,63 @@ const route = useRoute();
 const profile = ref<ProfileResponse>();
 
 const fetchProfile = async () => {
-  const { region, gameName, tagLine } = route.params;
-  const url = new URL(
-    `http://localhost:8080/api/v1/summoner/profile/by-riot-id/${region}/${gameName}/${tagLine}`,
-  );
+    const { region, gameName, tagLine } = route.params;
+    const url = new URL(
+        `${import.meta.env.VITE_API_BASE}/summoner/profile/by-riot-id/${region}/${gameName}/${tagLine}`,
+    );
 
-  try {
-    const res = await fetch(url);
+    try {
+        const res = await fetch(url);
 
-    if (!res.ok) {
-      router.push({
-        path: "/404",
-        query: { search: `${gameName}#${tagLine}`, region },
-      });
-      return;
+        if (!res.ok) {
+            router.push({
+                path: "/404",
+                query: { search: `${gameName}#${tagLine}`, region },
+            });
+            return;
+        }
+
+        const data: ProfileResponse = await res.json();
+
+        data.ranks.sort((a) => (a.queueType == "RANKED_SOLO_5x5" ? -1 : 1));
+
+        profile.value = data;
+    } catch (err) {
+        console.log(err);
     }
-
-    const data: ProfileResponse = await res.json();
-
-    data.ranks.sort((a) => (a.queueType == "RANKED_SOLO_5x5" ? -1 : 1));
-
-    profile.value = data;
-  } catch (err) {
-    console.log(err);
-  }
 };
 
 watch(() => route.params.gameName, fetchProfile, { immediate: true });
 </script>
 
 <template>
-  <div class="page">
-    <main v-if="profile" class="page__content">
-      <SummonerProfile
-        :summoner-data="profile"
-        :key="profile.summoner.accountId"
-      />
-      <MatchHistory :puuid="profile.summoner.puuid" />
-    </main>
-  </div>
+    <div class="page">
+        <main v-if="profile" class="page__content">
+            <SummonerProfile :summoner-data="profile" :key="profile.summoner.accountId" />
+            <MatchHistory :puuid="profile.summoner.puuid" />
+        </main>
+    </div>
 </template>
 
 <style scoped>
 .page {
-  min-height: 100vh;
-  background-color: var(--background-color);
-  padding: 0.5rem;
+    min-height: 100vh;
+    background-color: var(--background-color);
+    padding: 0.5rem;
 }
 
 .page__content {
-  height: 100%;
+    height: 100%;
 }
 
 @media screen and (min-width: 1024px) {
-  .page {
-    display: flex;
-    justify-content: center;
-  }
+    .page {
+        display: flex;
+        justify-content: center;
+    }
 
-  .page__content {
-    max-width: 60vw;
-  }
+    .page__content {
+        max-width: 60vw;
+    }
 }
 </style>
